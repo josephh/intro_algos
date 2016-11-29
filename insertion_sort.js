9e5f0f1efaa00ec815e5676360995a3d7b384237
josephh <josephh@rocketmail.com>
2016-11-29 08:51:45 +0000

    client code and empty merge sort

--- insertion_sort.js
+++ insertion_sort.js
@@ -3,14 +3,11 @@
 var assert = require('assert');
 
 // j is the input element index variable, key is the int at array index j and i stores the array index prior to key's index 
-let j = 1, i, key;
+let j = 0, i, key;
 
 module.exports = (inputArray) => {
   assert.equal(Object.prototype.toString.call(inputArray), '[object Array]', 'input parameter should be an object of type array');
   for(j; j < inputArray.length; j += 1) {
-    /** 
-     The for loop has an invariant - that the sub-array is sorted - and that must hold prior to the first loop iteration and after each iteration. 
-    */
     key = Number(inputArray[j]) // zero-based  
     i = j - 1  
 
