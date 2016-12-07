f426cafeafcc04a32efa518e529061ca07c1f5b3
josephh <josephh@rocketmail.com>
2016-12-07 12:01:26 +0000

    correct insertion sort

--- insertion_sort.js
+++ insertion_sort.js
@@ -2,26 +2,27 @@
 
 var assert = require('assert');
 
-// j is the input element index variable, key is the int at array index j and i stores the array index prior to key's index 
-let j = 0, i, key;
-
 module.exports = (inputArray) => {
+
+  // j is the input element index variable, key is the value at array index j and i stores the array index prior to key's index
+  let j = 0, i, key;
+
   assert.equal(Object.prototype.toString.call(inputArray), '[object Array]', 'input parameter should be an object of type array');
+
   for(j; j < inputArray.length; j += 1) {
 
-  key = Number(inputArray[j])
-    i = j - 1  
+    key = Number(inputArray[j])
+    i = j - 1
 
-    while(i >= -1 && Number(inputArray[i]) > key) { 
-    inputArray[i + 1] = inputArray[i]
-    i -= 1
+    while(i >= -1 && Number(inputArray[i]) > key) {
+      inputArray[i + 1] = inputArray[i]
+      i -= 1
     }
 
-  inputArray[i + 1] = key
+    inputArray[i + 1] = key
 
   }
 
   return inputArray;
 
 }
-
--- sort_client.js
+++ sort_client.js
@@ -3,6 +3,11 @@
 var insertionSort = require('./insertion_sort.js'),
   mergeSort = require('./merge_sort.js');
 
-let a = [2378,-32,43,4,123,3,423423,54,-1,23,20,0,100,0,213], aSorted = insertionSort(a);
+let input = process.argv[2].split(',')
+let a = input.slice(0, input.length / 2)
+console.log(`a? ${a}`)
+let b = input.slice(input.length / 2, input.length)
+console.log(`b? ${b}`)
 
-console.log(aSorted)
+console.log(`a sorted? ${insertionSort(a)}`)
+console.log(`b sorted? ${insertionSort(b)}`)
