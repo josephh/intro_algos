9b7b9e2290963fdd745357affa766a937603109e
josephh <josephh@rocketmail.com>
2016-12-08 19:35:21 +0000

    finish merge sort

--- merge_sort.js
+++ merge_sort.js
@@ -3,26 +3,36 @@
 var assert = require('assert');
 
 /**
- * Merge-sort is a recursive algorithm, which uses a helper merge function, which .
- * When the sequence has length 1 there's no more sorting to be done, so the recursion bottoms out.
+ * Recursive function to divide an array into 2 sorted subarrays and merge them (making use of an auxiliary method).
+ * The initial call to this function will be of the form mergeSort(Array, 1, Array.length).
+ * @param {Array} arr - the sequence containing (integer) items for merging.
+ * @param {Number} p - an index into the array (start).
+ * @param {Number} r - an index into the array (end).
  */
 function mergeSort(arr, p, r) {
 
     /**
     * Divides an n-element sequence into 2 subsequences (assumed to be already sorted) and merges them.
     * @param {Array} arr - the sequence containing 2 sorted subsequences for merging.
-    * @param p - an index into the array, such that p <= q < r. arr[p..q] is assumed to be sorted.
-    * @param q - an index into the array, such that p <= q < r.
-    * @param r - an index into the array, such that p <= q < r. arr[q+1..r] is assumed to be sorted.
+    * @param {Number} p - an index into the array, such that p <= q < r. arr[p..q] is assumed to be sorted.
+    * @param {Number} q - an index into the array, such that p <= q < r.
+    * @param {Number} r - an index into the array, such that p <= q < r. arr[q+1..r] is assumed to be sorted.
     */
     function mergeHelper(arr, p, q, r) {
       // the length of the subarrays
-      let newLeftArr = arr.slice(arr[p], arr[q]),
-      newRightArr = arr.slice(p + 1, r),
+      let newLeftArr = arr.slice(p - 1, q),
+      newRightArr = arr.slice(q, r),
       i = 0,
       j = 0;
-      for (let k = p; k < r; k++) {
-        if (newLeftArr[i] <= newRightArr[j]) {
+      /*
+       * 'Sentinel values' (in this case infinity) are added to the end of each array to guarantee the greater than or
+       lesser than operand returns the correct result.  It also avoids having to check for empty array or an index out
+       * of bounds, although it does preclude Inifinity being part of the values to sort.
+       */
+      newLeftArr.push(Infinity);
+      newRightArr.push(Infinity);
+      for (let k = p - 1; k < r; k++) {
+        if (Number(newLeftArr[i]) <= Number(newRightArr[j])) {
           arr[k] = newLeftArr[i];
           i += 1;
         } else {
@@ -33,11 +43,13 @@ function mergeSort(arr, p, r) {
     }
 
     if (p < r) {
-      let q = (p + r) / 2;
-      mergeHelper(arr, p, q);
-      mergeHelper(arr, q + 1, r);
-      mergeSort(arr, p, q, r);
+      let q = Math.floor((p + r) / 2);
+      mergeSort(arr, p, q);
+      mergeSort(arr, q + 1, r);
+      mergeHelper(arr, p, q, r); // finally put the 2 sorted arrays together
     }
+
+    return arr;
 
 }
 
