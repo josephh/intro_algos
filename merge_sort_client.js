595af72864e3b765f4836a0ea334da641d9dcae5
josephh <josephh@rocketmail.com>
2019-08-28 14:06:38 +0100

    tweak input example

--- merge_sort_client.js
+++ merge_sort_client.js
@@ -8,7 +8,7 @@ let input = process.argv[2] && process.argv[2].split(',') || null
 if(!input) {
   console.log(`invoke merge sort with a numeric, comma-separated input,
     for example:
-    node merge_sort_client 3,4,123,3,423423,54,-1,23,20,0,0,213,3476,-123,-23,8762349,99238123,-312,1,433`)
+    node merge_sort_client 33,4,123,3,423423,54,-1,23,20,0,0,213,3476,-123,-23,8762349,99238123,-312,1,433`)
 } else {
   console.log(`merged array? ${m.mergeSort(input, 1, input.length)}`);
 }
