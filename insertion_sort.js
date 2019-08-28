dd99a2d2d8d786162b999feca0467253f6b76ace
josephh <josephh@rocketmail.com>
2019-08-28 10:27:54 +0100

    better variable naming

--- insertion_sort.js
+++ insertion_sort.js
@@ -4,22 +4,28 @@ var assert = require('assert');
 
 module.exports = (inputArray) => {
 
-  // j is the input element index variable, key is the value at array index j and i stores the array index prior to key's index
-  let j = 0, i, key;
+  /**
+   *  j is the array index variable used to find n
+   *  n is the value being sorted in the outer loop iteration(s)
+   *  i is the array index variable used to traverse back to the array start
+   **/
+  let j = 0, i, n;
 
-  assert.equal(Object.prototype.toString.call(inputArray), '[object Array]', 'input parameter should be an object of type array');
+  assert.equal(Object.prototype.toString.call(inputArray),
+    '[object Array]',
+    'input parameter should be an object of type array');
 
   for(j; j < inputArray.length; j += 1) {
 
-    key = Number(inputArray[j])
+    n = Number(inputArray[j])
     i = j - 1
 
-    while(i >= -1 && Number(inputArray[i]) > key) {
+    while(i >= -1 && Number(inputArray[i]) > n) {
       inputArray[i + 1] = inputArray[i]
       i -= 1
     }
 
-    inputArray[i + 1] = key
+    inputArray[i + 1] = n
 
   }
 
